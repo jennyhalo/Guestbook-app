f39710f1b03c2b12ef6f8fd3f6fb5571840f0d15
Jenny-Carolina Halonen <jenny.halonen@outlook.com>
2023-04-26 19:18:37 +0300

    commit

--- public/logic/guestbook.js
+++ public/logic/guestbook.js
@@ -1,3 +1,4 @@
+// Haetaan jQueryllä data.json ja sieltä halutut datat appendataan guestbook taulukkoon
 $(function () {
   $.getJSON("data.json", function (data) {
     var message_data = "";
@@ -10,21 +11,4 @@ $(function () {
     });
     $("#table_body").append(message_data);
   });
-});
-// määritellään muuttuja(nappi) johon tulee toimintoja
-const backToTopBtn = document.getElementById("backToTopBtn");
-window.onscroll = function () {
-  scrollFunction();
-};
-// tässä functiossa määritellään, että nappi tulee näkyviin kun selataan sivua hieman alemmas, muuten se on näkymätön.
-function scrollFunction() {
-  if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
-    backToTopBtn.style.display = "block";
-  } else {
-    backToTopBtn.style.display = "none";
-  }
-}
-// Kun nappia painetaan, pääsee takaisin sivun alkuun
-backToTopBtn.addEventListener("click", () => {
-  document.documentElement.scrollTop = 0;
 });
